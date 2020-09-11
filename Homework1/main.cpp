298eb55ccfec26227d2e2bc4336b15aab771831b
kyleferrigan <Kyle.Ferrigan@iCloud.com>
2020-09-11 11:54:56 -0500

    Initial Commit

---
+++ Homework1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework1
+//
+//  Created by Kyle Ferrigan on 9/11/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
